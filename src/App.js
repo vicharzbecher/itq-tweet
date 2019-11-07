b92f9e8caf541df609e606b8e2bef40216166ca1
vicharzbecher <hugo.garcia@wizeline.com>
2019-11-07 16:36:37 -0600

    JSX vs React elements

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // return (
+  //   React.createElement('div', { className: "App" },
+  //     React.createElement('p', {}, "Hola")
+  //   )
+  // );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hola</p>
     </div>
   );
 }
