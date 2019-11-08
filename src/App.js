e1912c7066de722d9fbf3e9efc23d0de63c82a94
vicharzbecher <hugo.garcia@wizeline.com>
2019-11-07 18:08:07 -0600

    Loading components from fixed data array

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Message from './Message'
 
 function App() {
+
+  const messages = [
+    {
+      user: 'Yo',
+      text: 'Mi texto'
+    },
+    {
+      user: 'Tu',
+      text: 'Tu texto'
+    },
+    {
+      user: 'El',
+      text: 'Su texto'
+    },
+    {
+      user: 'Nosotros',
+      text: 'Nuestro texto'
+    },
+  ];
+
   return (
     <div>
       <h1 className="page-title">
         Tweeta
       </h1>
 
-      <Message user="Yo" text="Mi texto" />
-      <Message user="Tu" text="Tu texto" />
-      <Message user="El" text="Su texto" />
+      {messages.map((message) => {
+        return (
+          <Message user={message.user} text={message.text} />
+        );
+      })}
 
     </div>
   );
